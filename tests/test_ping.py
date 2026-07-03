3088231d0440c8f57df5e5c366221eac759e5399
Nick Sweet <nasweet@gmail.com>
2026-07-03 17:48:43 -0500

    build(T002): Create tests/test_ping.py with unittest.TestCase
    Task: T002
    Run: 2026-07-03T22-42-14Z

---
+++ tests/test_ping.py
@@ -0,0 +1,9 @@
+import unittest
+import sandbox_ping
+
+class TestPing(unittest.TestCase):
+    def test_ping(self):
+        self.assertEqual(sandbox_ping.ping(), "pong")
+
+if __name__ == '__main__':
+    unittest.main()
