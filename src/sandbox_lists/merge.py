b73f0d9c22a92529e37b5e1d41d58daa01dfa58b
Nick Sweet <nasweet@gmail.com>
2026-07-13 16:12:22 -0500

    build(T002): Create merge.py with merge_sorted function
    Task: T002
    Run: 2026-07-13T21-12-06Z

---
+++ src/sandbox_lists/merge.py
@@ -0,0 +1,14 @@
+def merge_sorted(a: list[int], b: list[int]) -> list[int]:
+    """Merge two sorted lists into one sorted list."""
+    result = []
+    i = j = 0
+    while i < len(a) and j < len(b):
+        if a[i] <= b[j]:
+            result.append(a[i])
+            i += 1
+        else:
+            result.append(b[j])
+            j += 1
+    result.extend(a[i:])
+    result.extend(b[j:])
+    return result
